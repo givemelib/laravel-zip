a1f11dae1a3873e45bd2f3fd0d574e8b41854f71
Davian Kohler <68523271+givemelib@users.noreply.github.com>
2020-07-22 12:21:54 +0700

    Add files via upload

---
+++ src/ZipFacade.php
@@ -0,0 +1,13 @@
+<?php
+namespace GiveMeLib\Zip;
+
+use Illuminate\Support\Facades\Facade;
+
+class ZipFacade extends Facade
+{
+
+    protected static function getFacadeAccessor()
+    {
+        return 'zip';
+    }
+}
